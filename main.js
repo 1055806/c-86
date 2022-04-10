cec9632763349033264ed7e1865aa97977935c33
1055806 <94760194+1055806@users.noreply.github.com>
2022-04-10 12:37:50 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL("BirthdayImage.jpg",function(img){
+    block_image_object = img;
+
+    block_image_object.scaleToWidth(700);
+    block_image_object.scaleToHeight(510);
+    block_image_object.set({
+        top:0,
+        left:0
+});
+canvas.add(block_image_object);	
+});
+}
+function playSound(){
+	x.play();
+}
